eecaf17651468f9227c92d7f4d7fbf253029a3bb
thehiddencoder <cs16btech11027@iith.ac.in>
2020-04-25 13:59:15 +0530

    Added a ride model

--- carpool/carpool/rider/models.py
+++ carpool/carpool/rider/models.py
@@ -1,4 +1,13 @@
 from django.db import models
 
 # Create your models here.
-#class ride(models.Model):
+class ride(models.Model):
+	userId = models.CharField(max_length = 200 , primary_key=True)
+	pickUp = models.CharField(max_length = 200)
+	destination = models.CharField(max_length = 200)
+	complete = models.CharField(max_length = 200)
+	status = models.CharField(max_length = 200)
+	cost = models.PositiveIntegerField(default = 0)
+
+	def __str__(self):
+		print(self.userId + " ---- " + self.destination)
